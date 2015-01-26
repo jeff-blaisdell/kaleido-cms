a46bccc9068e15bc9af2078bfa308f91ed2d8151
Jeff Blaisdell <jeff.blaisdell@gmail.com>
2015-01-25 19:34:13 -0600

    Generating Hippo CMS project.

---
+++ site/src/main/java/io/kaleido/cms/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package io.kaleido.cms.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="kaleidocms:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
